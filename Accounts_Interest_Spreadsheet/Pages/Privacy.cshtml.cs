40087f862c29a4cf0bdad99d0a27197fba92390f
nick v <nvalente.electrics@gmail.com>
2024-10-13 14:21:38 +0200

    created project using ASP.NET and Razor pages

---
+++ Accounts_Interest_Spreadsheet/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Accounts_Interest_Spreadsheet.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
