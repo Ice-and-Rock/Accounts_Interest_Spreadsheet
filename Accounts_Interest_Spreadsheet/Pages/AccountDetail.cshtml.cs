e5ad2884bcf938e1bdd5fab0a4194e35a45e00fe
nicholasvalente <nwvalente@hotmail.com>
2024-10-15 16:02:35 +0200

    Added bank name to URL address Build a small table for transactions using static data Had a good think about how to make this project scaleable with API’s and dynamic interest/transaction data.
    Ran out of time, for today.

--- Accounts_Interest_Spreadsheet/Pages/AccountDetail.cshtml.cs
+++ Accounts_Interest_Spreadsheet/Pages/AccountDetail.cshtml.cs
@@ -7,21 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Accounts_Interest_Spreadsheet.Pages
+
+// PLAN
+    // Change data to include transactionsIn/TransactionsOut to be more detailed.
+        // Research JSON formatting / LIST formatting to show transactions
+    // change TransactionsIn to be monthly
+    // change TransactionsOut to be monthly
+    // Create a new table w/initial mock data
+        // different colour from main details section
+
+
 {
     public class AccountDetailModel : PageModel
     {
         public BankAccountData Account { get; set; }
 
-        public void OnGet(int id)
+        public IActionResult OnGet(string bankName, int id)
         {
             // Fetch the account data based on the id
             var allAccounts = BankAccountData.GenerateDummyData();
-            Account = allAccounts.FirstOrDefault(a => a.Id == id); // Assuming you have Id property
-        }
-        private BankAccountData GetAccountById(int id)
-        {
-            // Logic to retrieve the account data based on the id from your data source
-            return new BankAccountData { /* initialize as needed */ };
+            Account = allAccounts.FirstOrDefault(a => a.Id == id); 
+
+            // does account exist? 
+            if (Account == null)
+            {
+                // retrun 404 error. Build proper error page later***
+                return NotFound(); 
+            }
+
+            return Page(); 
         }
     }
 }
